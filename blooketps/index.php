7fbe9900a5b6a32545d5679446021897985619a9
cheeto / bongus <41563327+cheeto2401@users.noreply.github.com>
2025-03-14 18:07:38 -0500

    more stuff
    dwdadawd

---
+++ blooketps/index.php
@@ -0,0 +1,27 @@
+<?php $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+?>
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Pack Opener</title> <!-- hello -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="packWrapper">
+        <div class="packContainer">
+            <button onclick="openPack()">Click to open a Pack!</button>
+            <span id="blookResult">Blook</span>
+        </div>
+    </div>
+    <script src="blooketPackOpener.js"></script>
+</body>
+</html> 
+<!-- hello, world!
+ tra la la! 
+
+-->
